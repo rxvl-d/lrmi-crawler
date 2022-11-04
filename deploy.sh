3e82a95adabaf304c2b7f5b04fbf6d8d9f8e5047
Ratan Sebastian <ratanjagan@gmail.com>
2022-11-04 14:24:59 +0100

    drop back down to 20 par

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ docker push rjsvaljean/lrmi-crawler:$1
 ssh devbox2 -- rm -f *.sif
 ssh devbox2 -- mkdir lrmi-crawler-out-cc lrmi-crawler-out-wdc19 lrmi-crawler-out-wdc20 lrmi-crawler-out-wdc21
 ssh devbox2 -- singularity pull docker://rjsvaljean/lrmi-crawler:$1
-cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/40/' | sed 's/SOURCE/wdc19/' > run_sbatch.sh
+cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/20/' | sed 's/SOURCE/wdc19/' > run_sbatch.sh
 scp run_sbatch.sh devbox2:run_sbatch_19.sh
 rm run_sbatch.sh
-cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/40/' | sed 's/SOURCE/wdc20/' > run_sbatch.sh
+cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/20/' | sed 's/SOURCE/wdc20/' > run_sbatch.sh
 scp run_sbatch.sh devbox2:run_sbatch_20.sh
 rm run_sbatch.sh
-cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/40/' | sed 's/SOURCE/wdc21/' > run_sbatch.sh
+cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/20/' | sed 's/SOURCE/wdc21/' > run_sbatch.sh
 scp run_sbatch.sh devbox2:run_sbatch_21.sh
 rm run_sbatch.sh
 ssh devbox2 -- sbatch -c 10 -J lrmi19 -o output_19.log -e err_19.log run_sbatch_19.sh

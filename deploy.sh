0c7a3fbbd920b23dab071ec87c7c3eef118b237d
Ratan Sebastian <ratanjagan@gmail.com>
2022-11-23 10:36:47 +0100

    20 processes feels about right

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ ssh devbox2 -- 'rm /nfs/data/webdatacommons/out/2019/*.gz'
 ssh devbox2 -- 'rm /nfs/data/webdatacommons/out/2020/*.gz'
 ssh devbox2 -- 'rm /nfs/data/webdatacommons/out/2020/*.gz'
 ssh devbox2 -- singularity pull docker://rjsvaljean/lrmi-crawler:$1
-cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/40/' | sed 's/SOURCE/2019/' > run_sbatch.sh
+cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/20/' | sed 's/SOURCE/2019/' > run_sbatch.sh
 scp run_sbatch.sh devbox2:run_sbatch_19.sh
 rm run_sbatch.sh
-cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/40/' | sed 's/SOURCE/2020/' > run_sbatch.sh
+cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/20/' | sed 's/SOURCE/2020/' > run_sbatch.sh
 scp run_sbatch.sh devbox2:run_sbatch_20.sh
 rm run_sbatch.sh
-cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/40/' | sed 's/SOURCE/2021/' > run_sbatch.sh
+cat run_sbatch.sh.template | sed "s/SIF_FILE/lrmi-crawler_$1.sif/" | sed 's/N_CORES/20/' | sed 's/SOURCE/2021/' > run_sbatch.sh
 scp run_sbatch.sh devbox2:run_sbatch_21.sh
 rm run_sbatch.sh
 ssh devbox2 -- sbatch -c 10 -J lrmi19 -o output_19.log -e err_19.log run_sbatch_19.sh
